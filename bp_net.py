767c8ee4d807133caa3a83beb84dc737413096a5
ykn <3486341697@qq.com>
2025-12-24 20:29:54 +0800

    YKN

--- bp_net.py
+++ bp_net.py
@@ -12,6 +12,8 @@ data = load_breast_cancer()
 X = data.data          # (569, 30)
 y = data.target        # 0/1
 
+HIDDEN_UNITS = 65       #隐藏层神经元个数
+LEARNING_RATE = 0.1   # 学习率
 # 划分训练集 / 测试集
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0
@@ -34,9 +36,9 @@ y_test  = torch.tensor(y_test, dtype=torch.long)
 class BPNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(30, 16)   # 输入层 -> 隐藏层
+        self.fc1 = nn.Linear(30, HIDDEN_UNITS)   # 输入层 -> 隐藏层
         self.relu = nn.ReLU()          # 激活函数
-        self.fc2 = nn.Linear(16, 2)    # 输出层（二分类）
+        self.fc2 = nn.Linear(HIDDEN_UNITS, 2)    # 输出层（二分类）
 
     def forward(self, x):
         x = self.fc1(x)    # z = W1x + b1
@@ -50,7 +52,7 @@ model = BPNet()
 # 3. 损失函数 & 优化器
 # =============================
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 # =============================
 # 4. 训练（BP 发生在这里）

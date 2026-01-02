5940d2bd28fdece62a93c274a875bd1eb0c52367
4455dd2 <1040860923@qq.com>
2026-01-02 23:09:34 +0800

    Update bp_net.py

--- bp_net.py
+++ bp_net.py
@@ -34,26 +34,21 @@ y_test  = torch.tensor(y_test, dtype=torch.long)
 class BPNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(30, 16)   # 输入层 -> 隐藏层
+        self.fc1 = nn.Linear(30, 64)   # 输入层 -> 隐藏层
         self.relu = nn.ReLU()          # 激活函数
-        self.fc2 = nn.Linear(16, 2)    # 输出层（二分类）
+        self.fc2 = nn.Linear(64, 2)    # 输出层（二分类）
 
     def forward(self, x):
         x = self.fc1(x)    # z = W1x + b1
-        x = self.relu(x)   # a = f(z)
-        x = self.fc2(x)    # 输出层
-        return x
-
-model = BPNet()
-
-# =============================
+	@@ -50,7 +50,7 @@ def forward(self, x):
 # 3. 损失函数 & 优化器
 # =============================
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=0.1)
 
 # =============================
 # 4. 训练（BP 发生在这里）
+	
 # =============================
 epochs = 200
 for epoch in range(1, epochs + 1):
@@ -81,3 +76,4 @@ with torch.no_grad():
     test_acc = (pred == y_test).float().mean().item()
 
 print("Test Accuracy =", test_acc)
+
